208871e7cd3589e75604c8cf07ed05fe6d55cfa0
JoeWOp <95669513+JoeWOp@users.noreply.github.com>
2021-12-13 17:56:25 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+const keys = [];
+
+const player = {  
+    x: 0,
+    y: 0,
+    width: ???,
+    height: ???,
+    frameX: 0,
+    frameY: 0,
+    speed: 9,
+    moving: false   
+};
+
+const playerSprite = new Image();
+playerSprite.src = "...";
